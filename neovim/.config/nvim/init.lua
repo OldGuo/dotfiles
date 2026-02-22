7c214976a7e7eb01b48ddd34e9c9660e35627792
youngxguo <young.guo@gmail.com>
2026-02-21 21:08:30 -0800

    nvim: set mapleader before plugin mappings

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,4 +1,6 @@
 -- Suppress nvim-lspconfig deprecation warning
+vim.g.mapleader = " "
+
 local notify = vim.notify
 vim.notify = function(msg, ...)
   if msg:match("nvim%-lspconfig.*deprecated") then
